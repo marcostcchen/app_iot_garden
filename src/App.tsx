aeaaccd7ce8afec4255d4dbb4850341618e58310
Marcos Chen <marcostcchen@gmail.com>
2021-09-17 19:01:34 -0300

    feature/refactorCode - navigation components refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,49 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import * as Views from './views';
 import SplashScreen from 'react-native-splash-screen';
 import { Root, Badge } from 'native-base';
-import { sqlLiteThenFunctionQuery } from './utils';
 import { TouchableOpacity, Image, Text, LogBox } from 'react-native';
 import { View } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import { MainStackNavigation } from './navigation';
 
-const Stack = createStackNavigator();
+export const App: React.FC = () => {
+  const [badgeValue, setBadgeValue] = useState(0)
 
-interface Props { }
-
-interface State {
-  badgeValue: number,
-}
-
-export default class App extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props)
-    this.state = {
-      badgeValue: 0,
-    }
-  }
-
-  componentDidMount = async () => {
+  useEffect(() => {
     SplashScreen.hide();
-    await this.createTablesSqlLite();
-    await this.updateBadge();
-  }
+    updateBadge();
+  }, [])
 
-  render() {
-    return (
-      <Root>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Login" component={Views.LoginScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Iot Garden" component={Views.HomeScreen}
-              options={({ navigation, route }) => ({ headerLeft: null, headerTintColor: 'green', headerRight: () => this.headerRight(navigation, route) })}
-            />
-            <Stack.Screen name="Detalhes Planta" component={Views.DetalhesPlantaScreen} options={{ headerShown: true, headerTintColor: 'green' }} />
-            <Stack.Screen name="Notificações" component={Views.NotificacoesScreen} options={{ headerShown: true, headerTintColor: 'green' }} />
-            <Stack.Screen name="Configuração da Planta" component={Views.ConfiguracaoPlantaScreen} options={{ headerShown: true, headerTintColor: 'green' }} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </Root>
-    )
-  }
 
-  headerRight = (navigation, route) => (
+  const headerRight = (navigation, route) => (
     <View style={{ width: '100%', flexDirection: 'row' }}>
-      <TouchableOpacity style={{ height: 20, width: 20, marginRight: 20, marginTop: 5 }} onPress={this.updateBadge}>
+      <TouchableOpacity style={{ height: 20, width: 20, marginRight: 20, marginTop: 5 }} onPress={updateBadge}>
         <Image style={{ height: '100%', width: '100%' }} source={require('./images/refreshButtonDark.png')} />
       </TouchableOpacity>
       <TouchableOpacity style={{ height: 20, width: 20, marginRight: 20, marginTop: 5 }} onPress={() => navigation.navigate("Notificações")}>
         <Image style={{ height: '100%', width: '100%' }} source={require('./images/notificationIcon.png')} />
         <Badge style={{ position: "absolute", right: -5, top: -5, height: 20, width: 20 }} warning >
-          <Text>{this.state.badgeValue}</Text>
+          <Text>{badgeValue}</Text>
         </Badge>
       </TouchableOpacity>
     </View>
   )
 
-  updateBadge = async () => {
+  const updateBadge = async () => {
     let badgeValue = await AsyncStorage.getItem("@badgeValue");
     if (badgeValue) {
-      this.setState({ badgeValue: Number(badgeValue) });
+      setBadgeValue(Number(badgeValue));
     }
   }
 
-  createTablesSqlLite = async () => {
-    const createValidadoQuery = 'CREATE TABLE IF NOT EXISTS ConfigTable(planta VARCHAR(255), tempMax VARCHAR(255), tempMin VARCHAR(255), umidMax VARCHAR(255), umidMin VARCHAR(255), umidSoloMax VARCHAR(255), umidSoloMin VARCHAR(255))';
-    await sqlLiteThenFunctionQuery(createValidadoQuery, [], null);
-
-    // Clean notification table
-    const deleteNotificationQuery = 'DELETE FROM NotificationTable';
-    await sqlLiteThenFunctionQuery(deleteNotificationQuery, [], null);
-
-    const createNotificationQuery = 'CREATE TABLE IF NOT EXISTS NotificationTable(planta VARCHAR(255), message VARCHAR(255))';
-    await sqlLiteThenFunctionQuery(createNotificationQuery, [], null);
-  }
+  return (
+    <Root>
+      <NavigationContainer>
+        <MainStackNavigation />
+      </NavigationContainer>
+    </Root>
+  )
 }
 
