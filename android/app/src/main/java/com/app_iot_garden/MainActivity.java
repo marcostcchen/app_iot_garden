8d67020e4759d701c1728c1fdd41b2afb2866d4d
Marcos Chen <marcostcchen@gmail.com>
2021-09-26 17:01:50 -0300

    App - fixed splash icons

--- android/app/src/main/java/com/app_iot_garden/MainActivity.java
+++ android/app/src/main/java/com/app_iot_garden/MainActivity.java
@@ -1,6 +1,8 @@
 package com.app_iot_garden;
 
+import android.os.Bundle; // import this
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // import this
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +14,10 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "app_iot_garden";
   }
+  
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 }
